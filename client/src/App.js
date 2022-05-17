d43c82a4bb78fdcd0bf33ed59f835eaf044110cd
Lior Azar <lior87@gmail.com>
2022-05-17 12:22:24 +0300

    set the context of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
 import './App.css';
+import AuthProvider from './components/AuthProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+
+      </AuthProvider>
     </div>
   );
 }
