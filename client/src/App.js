2f0a38f42c98a5c81e9356548ff28684b0175e6e
Lior Azar <lior87@gmail.com>
2022-05-22 16:39:56 +0300

    rollback to web react

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import logo from './logo.svg';
 import './App.css';
-import AuthProvider from './components/AuthProvider';
 
 function App() {
   return (
-    <div>
-      <AuthProvider>
-
-      </AuthProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
